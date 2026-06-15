ed58166ff897c5daf602f7bc12b1e527321b8d62
Lasse Aagren <lassea@unity3d.com>
2026-06-15 12:19:32 +0200

    feat: open PR in browser when notification is clicked
    Deliver notifications via NSUserNotificationCenter through a small CGO
    shim, attaching the PR URL to userInfo. The delegate opens the URL via
    NSWorkspace on activation and removes the delivered notification so it
    clears from Notification Center. Each notification gets a unique
    identifier so multiple events stack rather than replace.

--- go.mod
+++ go.mod
@@ -4,7 +4,6 @@ go 1.25.7
 
 require (
 	github.com/BurntSushi/toml v1.6.0
-	github.com/gen2brain/beeep v0.11.2
 	github.com/getlantern/systray v1.2.2
 	github.com/google/go-github/v72 v72.0.0
 	github.com/urfave/cli/v3 v3.9.0
@@ -12,9 +11,7 @@ require (
 )
 
 require (
-	git.sr.ht/~jackmordaunt/go-toast v1.1.2 // indirect
 	github.com/cespare/xxhash/v2 v2.3.0 // indirect
-	github.com/esiqveland/notify v0.13.3 // indirect
 	github.com/getlantern/context v0.0.0-20220418194847-3d5e7a086201 // indirect
 	github.com/getlantern/errors v1.0.4 // indirect
 	github.com/getlantern/golog v0.0.0-20230503153817-8e72de7e0a65 // indirect
@@ -23,16 +20,9 @@ require (
 	github.com/getlantern/ops v0.0.0-20231025133620-f368ab734534 // indirect
 	github.com/go-logr/logr v1.4.3 // indirect
 	github.com/go-logr/stdr v1.2.2 // indirect
-	github.com/go-ole/go-ole v1.3.0 // indirect
 	github.com/go-stack/stack v1.8.1 // indirect
-	github.com/godbus/dbus/v5 v5.2.2 // indirect
 	github.com/google/go-querystring v1.2.0 // indirect
-	github.com/jackmordaunt/icns/v3 v3.0.1 // indirect
-	github.com/nfnt/resize v0.0.0-20180221191011-83c6a9932646 // indirect
 	github.com/oxtoacart/bpool v0.0.0-20190530202638-03653db5a59c // indirect
-	github.com/sergeymakinen/go-bmp v1.0.0 // indirect
-	github.com/sergeymakinen/go-ico v1.0.0 // indirect
-	github.com/tadvi/systray v0.0.0-20190226123456-11a2b8fa57af // indirect
 	go.opentelemetry.io/auto/sdk v1.2.1 // indirect
 	go.opentelemetry.io/otel v1.44.0 // indirect
 	go.opentelemetry.io/otel/metric v1.44.0 // indirect
--- internal/notify/notify.go
+++ internal/notify/notify.go
@@ -2,11 +2,70 @@
 // gated by the user's per-event preferences in config.NotificationConfig.
 package notify
 
+/*
+#cgo CFLAGS: -x objective-c -fobjc-arc -Wno-deprecated-declarations
+#cgo LDFLAGS: -framework Cocoa
+
+#import <Cocoa/Cocoa.h>
+
+@interface GHNotifyDelegate : NSObject <NSUserNotificationCenterDelegate>
+@end
+
+@implementation GHNotifyDelegate
+- (void)userNotificationCenter:(NSUserNotificationCenter *)center
+        didActivateNotification:(NSUserNotification *)notification {
+    NSString *u = notification.userInfo[@"url"];
+    if (u.length > 0) {
+        NSURL *nsurl = [NSURL URLWithString:u];
+        if (nsurl) {
+            [[NSWorkspace sharedWorkspace] openURL:nsurl];
+        }
+    }
+    [center removeDeliveredNotification:notification];
+}
+
+- (BOOL)userNotificationCenter:(NSUserNotificationCenter *)center
+     shouldPresentNotification:(NSUserNotification *)notification {
+    return YES;
+}
+@end
+
+static GHNotifyDelegate *gDelegate = nil;
+
+static void ghnEnsureDelegate(void) {
+    if (gDelegate == nil) {
+        gDelegate = [[GHNotifyDelegate alloc] init];
+    }
+    NSUserNotificationCenter *c = [NSUserNotificationCenter defaultUserNotificationCenter];
+    if (c.delegate != gDelegate) {
+        c.delegate = gDelegate;
+    }
+}
+
+static void ghnDeliver(const char *title, const char *body, const char *url) {
+    NSString *t = title != NULL ? [NSString stringWithUTF8String:title] : @"";
+    NSString *b = body != NULL  ? [NSString stringWithUTF8String:body]  : @"";
+    NSString *u = (url != NULL && url[0] != '\0') ? [NSString stringWithUTF8String:url] : nil;
+
+    dispatch_async(dispatch_get_main_queue(), ^{
+        ghnEnsureDelegate();
+        NSUserNotification *n = [[NSUserNotification alloc] init];
+        n.title = t;
+        n.informativeText = b;
+        if (u != nil) {
+            n.userInfo = @{@"url": u};
+        }
+        n.identifier = [[NSUUID UUID] UUIDString];
+        [[NSUserNotificationCenter defaultUserNotificationCenter] deliverNotification:n];
+    });
+}
+*/
+import "C"
+
 import (
 	"fmt"
 	"log"
-
-	"github.com/gen2brain/beeep"
+	"unsafe"
 
 	"github.com/boyvinall/ghnotify/internal/config"
 	"github.com/boyvinall/ghnotify/internal/github"
@@ -41,9 +100,22 @@ func (n *Notifier) dispatch(c poller.Change) {
 	if !ok {
 		return
 	}
-	if err := beeep.Notify(title, body, ""); err != nil {
+	if err := osNotify(title, body, c.PR.URL); err != nil {
 		log.Printf("ghnotify: notification: %v", err)
 	}
+}
+
+// osNotify delivers a desktop notification via NSUserNotificationCenter that,
+// when clicked, opens url in the default browser.
+func osNotify(title, body, url string) error {
+	cTitle := C.CString(title)
+	defer C.free(unsafe.Pointer(cTitle))
+	cBody := C.CString(body)
+	defer C.free(unsafe.Pointer(cBody))
+	cURL := C.CString(url)
+	defer C.free(unsafe.Pointer(cURL))
+	C.ghnDeliver(cTitle, cBody, cURL)
+	return nil
 }
 
 func (n *Notifier) format(c poller.Change) (title, body string, ok bool) {
